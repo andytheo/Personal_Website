94ffd372da7a62930c5e827c50c54b5db779c139
andytheo <igbeinkutuandrew@yahoo.com>
2023-01-07 22:32:06 -0500

    Deployed to vercel

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-import Profile from './Profile';
-import Projects from './Projects';
-import Blog from './components/blog/Blogposts';
-import Switching from './components/blog/SwitchingCareer';
-import Learn from './components/blog/Learn';
-import Contact from './components/contact/ContactClone';
-
-
-import { NavLink, Switch, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Projects from "./Projects";
+import Blog from "./components/blog/Blogposts";
+import Switching from "./components/blog/SwitchingCareer";
+import Learn from "./components/blog/Learn";
+import Contact from "./components/contact/ContactClone";
+import { NavLink, Switch, Route } from "react-router-dom";
 
 function Main() {
-  return(
+  return (
     <Switch>
-    <Route exact path='/' component={Home}></Route>
-    <Route exact path='/profile' component={Profile}></Route>
-    <Route exact path='/projects' component={Projects}></Route>
-    <Route exact path='/blog' component={Blog}></Route>
-    <Route exact path='/blog-switching-career' component={Switching}></Route>
-    <Route exact path='/blog-learn' component={Learn}></Route>
-    <Route exact path='/contact' component={Contact}></Route>
-  </Switch>
-  )
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/profile" component={Profile}></Route>
+      <Route exact path="/projects" component={Projects}></Route>
+      <Route exact path="/blog" component={Blog}></Route>
+      <Route exact path="/blog-switching-career" component={Switching}></Route>
+      <Route exact path="/blog-learn" component={Learn}></Route>
+      <Route exact path="/contact" component={Contact}></Route>
+    </Switch>
+  );
 }
 
 function App() {
   return (
     <div>
-    <Main />
+      <Main />
     </div>
-  )
+  );
 }
 
 export default App;
